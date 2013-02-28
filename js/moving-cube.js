6631d37348dc734ac1fa3146a39fec9a0bade631
Nicklas Nyström <nirivonsiri@gmail.com>
2013-02-28 08:40:05 +0100

    MovingCubes can now detect collisions

--- js/moving-cube.js
+++ js/moving-cube.js
@@ -11,6 +11,7 @@ material.color.setHex(0xff0000);
  */
 MovingCube = function() {
     this.selected = false;
+    this.obstacles;
 }
 
 MovingCube.prototype = new THREE.Mesh(geometry, material);
@@ -36,6 +37,12 @@ MovingCube.prototype.isSelected = function() {
 
 
 /**
+ */
+MovingCube.prototype.setObstacles = function(obstacles) {
+    this.obstacles = obstacles;
+}
+
+/**
  Move (at most) the specified distance toward the specified point
  */
 MovingCube.prototype.moveTowardPoint = function(point, dx, dz) {
@@ -44,27 +51,64 @@ MovingCube.prototype.moveTowardPoint = function(point, dx, dz) {
         // MOVE TO THE CORRECT POSITION
     } else {
         // Translate by dx in the specified direction
-        this.translateX(((this.position.x < point.x) ? dx : -dx));
-        
+        var xMovement = ((this.position.x < point.x) ? dx : -dx);
+        this.translateX(xMovement);
+        if (this.collides()) {
+            this.translateX(-xMovement);
+        }
     }
     
     // Z movement
     if (Math.abs(this.position.z - point.z) < dz)  {
         // MOVE TO THE CORRECT POSITION
-        // Translate by dx in the specified direction
     } else {
-        // Translate by dx in the specified direction
-        this.translateZ(((this.position.z < point.z) ? dz : -dz));
+        // Translate by dy in the specified direction
+        var zMovement = ((this.position.z < point.z) ? dz : -dz);
+        this.translateZ(zMovement);
+        if (this.collides()) {
+            this.translateZ(-zMovement);
+        }
     }
 }
 
 
+
 /**
  Check whether a collision with an object in the array 'objects'
- would occur of the cube snapped to the specified location
+ would occur of the cube snapped to the specified offset
+
+MovingCube.prototype.collidesAtOffset = function(dx, dy, dz) {
+    return false;
+    console.log(position);
+    var currentPosition = this.position;
+    this.position.set(position);
+    var collides = this.collides();
+    this.position.set(currentPosition);
+    return collides;
+} */
+
+
+/**
+ Check if the cube is overlapping any obstacles
  */
-MovingCube.prototype.collidesAtPosition = function(objects, position) {
-    // Check!
+MovingCube.prototype.collides = function() {
+    var collides = false;
+    var offset = CUBE_SIDE / 2;
+    var obstacle;
+    
+    for (var i = 0; i < this.obstacles.length; i++) {
+        obstacle = obstacles[i]
+        if (!(
+              this.position.x + offset < obstacle.position.x - offset ||
+              this.position.x - offset > obstacle.position.x + offset ||
+              
+              this.position.z + offset < obstacle.position.z - offset ||
+              this.position.z - offset > obstacle.position.z + offset
+              )) {
+            collides = true;
+        }
+    }
+    return collides;
 }
 
 
