ddf6100f61a7686d6921886ab0c565de3295a830
Nicklas <nirivonsiri@gmail.com>
2013-03-28 13:04:46 +0100

    Klotski now fetches levels from a separate file.

---
+++ js/klotski-levels.js
@@ -0,0 +1,14 @@
+function KlotskiLevels() {};
+
+KlotskiLevels.level1 = [new KlotskiToken(0, 0, 2, 1),
+                        new KlotskiToken(2, 0, 2, 1),
+                        new KlotskiToken(0, 1, 2, 1),
+                        new KlotskiToken(0, 2, 2, 1),
+                        new KlotskiToken(2, 2, 2, 1),
+                        new KlotskiToken(0, 3, 1, 1),
+                        new KlotskiToken(1, 3, 1, 1),
+                        new KlotskiToken(0, 4, 1, 1),
+                        new KlotskiToken(1, 4, 1, 1),
+                        new KlotskiToken(2, 3, 2, 2, true) // Main block
+                        ];
+
