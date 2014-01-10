0216137c83e741904912ba80337dcec212010ba6
jimnys-8 <snaggadhagga@gmail.com>
2014-01-10 09:45:25 +0100

    Fixed and extended engine, plus style upgrade

--- public/js/bamse.js
+++ public/js/bamse.js
@@ -1,110 +1,115 @@
 
 var bamse = {
 	
+    title : "Bamse världens starkaste björn",
+    
 	src : { src:"http://www.youtube.com/watch?v=DHdGjldEukQ", type:"video/youtube" },
 		  /*[{src : "/videos/bamse.ogg", type : "video/ogg"},
 		   {src : "/videos/bamse.mp4", type : "video/mp4"}],*/
-	/*
-	events : {
-		"0.0" : {
-			type : "set-background",
-			data : {
-				name : "trollskogen",
-			},
-		},
-		"5.0" : {
-			type : "add-character",
-			data : {
-				name : "skalman",
-				receiver : "random", // all/first/last ???
-				response : {
-					accepts : "return",
-					until : "10.0",
-				}
-			},
-		},
-		"10.0" : {
-			type : "remove-character",
-			data : {
-				name : "skalman",
-			},
-		},
-	},
-	*/
 	
 	events : [
-			  {
+			  /*{
 			  time : 0.0,
 			  type : "set-background",
 			  data : { name : "trollskogen", },
+			  },*/
+              
+              {
+			  time : 1.0,
+			  type : "add-object",
+			  data : {
+                sprite:"title",
+                x : 400,
+                y : 300,
+                scale : 0,
+                actions : [
+                           {name : "Scale", args:[1, 2]},
+                           {name : "Rotate", args:[720, 2]},
+                           ]
+                },
 			  },
+              
 			  {
-			  time : 10.0,
+			  time : 5.0,
 			  type : "add-character",
 			  data : {
 				name : "skalman",
-				responseTime:10,
+				responseTime:5,
 				responseType:"click",
 				responseId:"123", // this will be set by the dispatcher
+                },
 			  },
-			  },
-			  {
-			  time : 20.0,
+			  
+              {
+			  time : 10.0,
 			  type : "remove-character",
 			  data : { name : "skalman", },
 			  },
+              
+              {
+			  time : 15.0,
+			  type : "add-character",
+			  data : {
+                name : "lilleskutt",
+                responseTime:15,
+                responseType:"click",
+                },
+			  },
+             
+              {
+			  time : 20.0,
+			  type : "add-character",
+			  data : {
+                name : "bamse",
+                responseTime:10,
+                responseType:"click",
+                },
+			  },
+			  
+              {
+			  time : 30.0,
+			  type : "remove-character",
+			  data : { name : "bamse", },
+			  },
+			  
+              {
+			  time : 30.0,
+			  type : "remove-character",
+			  data : { name : "lilleskutt", },
+			  },
 			  ],
 	
-	/*
-	events : [{
-			  time : 0.4,
-			  type : "show-graphics",
-			  // ...
-			  }],
-	*/
-	
-	backgroundsBaseURL : "/images/bamse/backgrounds/",
 	backgrounds : {
-		// "_baseURL" : "/images/bamse/backgrounds/",
-		"trollskogen" : "trollskogen.jpg",
+		// "urlBase" : "/images/bamse/backgrounds/",
+		"trollskogen" : "/images/bamse/backgrounds/trollskogen.jpg",
 	},
+    
+    sprites : {
+        // "urlBase" : "/images/bamse/",
+        "bamse" : "/images/bamse/bamse.png",
+        "skalman" : "/images/bamse/skalman.png",
+        "lilleskutt" : "/images/bamse/lilleskutt.png",
+        "title" : "/images/bamse/bamse-title.gif",
+    },
 	
-	characters : {
-		/*"_all" : {
-			behavior : [ "gravity", "draggable", ],
-		},*/
+    characters : {
 		"bamse" : {
-			image : "/images/bamse/bamse.png",
+			sprite : "bamse",
+            speed : 5,
+            actions : [
+                       {name : "Bounce"},
+                       {name : "Move"},
+                       {name : "Rotate", args:[{speed:180}, -1]},
+                       ],
+            // x:y:dx:dy:rotation:scale:alpha:
 		},
 		"skalman" : {
-			image : "/images/bamse/skalman.png",
-			/*
-			spritesBaseURL : "/images/bamse/",
-			sprites : {
-				"_default" : "skalman.png",
-				"walk" : [ "skalmanWalk1.png", "skalmanWalk2.png", "skalmanWalk3.png" ],
-				"stand" : "skalman.png",
-			},
-			behavior : [ "gravity", "walk", "...", ],
-			actions : {
-				"walk" : {
-					sprite : "walk"
-				},
-			},
-			 */
+            alpha : 0,
+			sprite : "skalman",
+            actions : [ {name : "FadeIn", args:[1]} ],
 		},
 		"lilleskutt" : {
-			image : "/images/bamse/lilleskutt.png",
-		},
-	},
-};
-
-/*
-for (var i in bamse.events) {
-	bamse.events[i].id = (Math.random() * 10e16).toFixed(0).toString();
-}
- */
-
-
-
-
+			sprite : "lilleskutt",
+            x : -100,
+            y : -100,
+            dx : 4,
